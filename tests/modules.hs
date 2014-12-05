f12e1df91fdfbe676c48381ebe359881f7d05ba9
Oswyn Brent <oswyn.brent@anchor.com.au>
2014-12-05 16:16:20 +1100

    Moved implementation from tests to lib

--- tests/modules.hs
+++ tests/modules.hs
@@ -1,7 +1,5 @@
-{-# LANGUAGE FlexibleContexts           #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE OverloadedStrings          #-}
-{-# LANGUAGE RecordWildCards            #-}
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 -- | Description: Test git repository setup.
 module Main where
@@ -11,43 +9,14 @@ import           Control.Monad.Base
 import           Control.Monad.IO.Class      ()
 import           Control.Monad.Trans.Control
 import           Data.Monoid
-import           Data.String
-import           Data.String.Utils
-import           Data.Text                   (Text)
-import qualified Data.Text                   as T
 import           System.Exit
 import           System.FilePath.Posix
 import           System.Posix.Temp
 import           System.Process
 import           Test.Hspec
 
-
-data Check
-data Fix
-
-data Status a
-    = Success ModuleName Text
-    | Failure ModuleName Text
-    | Catastrophe ModuleName Text
-  deriving (Show, Ord, Eq)
-
-colorize :: Status a -> Text
-colorize (Success     (ModuleName x) y) = "\x1b[32m" <> x <> " succeeded with " <> y <> "\x1b[0m"
-colorize (Failure     (ModuleName x) y) = "\x1b[33m" <> x <> " failed with "    <> y <> "\x1b[0m"
-colorize (Catastrophe (ModuleName x) y) = "\x1b[31m" <> x <> " exploded with "  <> y <> "\x1b[0m"
-
-newtype ModulePath = ModulePath {
-    unModulePath :: FilePath
-} deriving (Show, Ord, Eq, IsString)
-
-newtype ModuleName = ModuleName {
-    unModuleName :: Text
-} deriving (Show, Ord, Eq, IsString, Monoid)
-
-data ModuleExecutorImpl m = ModuleExecutorImpl{
-    executeFix   :: ModulePath -> m (Status Fix),
-    executeCheck :: ModulePath -> m (Status Check)
-}
+import           Git.Vogue.Modules
+import           Git.Vogue.Types
 
 failingModuleExecutorImpl :: ModuleExecutorImpl IO
 failingModuleExecutorImpl =
@@ -58,31 +27,6 @@ succeedingModuleExecutorImpl :: ModuleExecutorImpl IO
 succeedingModuleExecutorImpl =
         ModuleExecutorImpl (const . return $ Success mempty mempty)
                            (const . return $ Success mempty mempty)
-
-checkModules
-    :: Monad m
-    => ModuleExecutorImpl m
-    -> [ModulePath]
-    -> m (Status Check)
-checkModules ModuleExecutorImpl{..} ps = do
-    rs <- mapM executeCheck ps
-    return $ insertMax rs (T.unlines $ map colorize rs)
-
-fixModules
-    :: Monad m
-    => ModuleExecutorImpl m
-    -> [ModulePath]
-    -> m (Status Fix)
-fixModules ModuleExecutorImpl{..} ps = do
-    rs <- mapM executeFix ps
-    return $ insertMax rs (T.unlines $ map colorize rs)
-
-insertMax :: [Status a] -> Text -> Status a
-insertMax rs txt =
-    case maximum rs of
-        Success{} -> Success mempty txt
-        Failure{} -> Failure mempty txt
-        Catastrophe{} -> Catastrophe mempty txt
 
 main :: IO ()
 main = hspec $ do
@@ -115,25 +59,6 @@ main = hspec $ do
         it "check fails on exploding module" $
             runCheckExecutor "exploding"
                              (Catastrophe "exploding" "half-life 3 confirmed\n")
-
-ioModuleExecutorImpl :: ModuleExecutorImpl IO
-ioModuleExecutorImpl =
-    ModuleExecutorImpl (f "fix") (f "check")
-  where
-    f arg (ModulePath path) = do
-        name <- getName path
-        (status, stdout, stderr) <- readProcessWithExitCode path [arg] mempty
-        let glommed = fromString $ stdout <> stderr
-        return $ case status of
-            ExitSuccess -> Success name glommed
-            ExitFailure 1 -> Failure name glommed
-            ExitFailure _ -> Catastrophe name glommed
-
-    getName path = do
-        (status, name, _) <- readProcessWithExitCode path ["name"] mempty
-        return . ModuleName . fromString . strip $ case status of
-            ExitSuccess -> if null name then path else name
-            ExitFailure _ -> path
 
 runCheckExecutor :: FilePath -> Status Check -> Expectation
 runCheckExecutor = runTestExecutor executeCheck
