d1336fc8523a0261dd4242d53fb11c89fa9fbaf1
Christian Marie <christian@ponies.io>
2015-01-05 09:43:28 +1100

    Update/add licenses

--- tests/plugins.hs
+++ tests/plugins.hs
@@ -1,5 +1,5 @@
 --
--- Copyright © 2013-2014 Anchor Systems, Pty Ltd and Others
+-- Copyright © 2013-2015 Anchor Systems, Pty Ltd and Others
 --
 -- The code in this file, and the program it is a part of, is
 -- made available to you by its authors as open source software:
