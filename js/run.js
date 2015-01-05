ab2eb93b17e704fb408028de5997cff49e9f789c
Geoffrey Roberts <groberts@anchor.net.au>
2015-01-05 14:04:21 +1100

    Initial commit

---
+++ js/run.js
@@ -0,0 +1,25 @@
+$(document).ready( function() {
+	var activate = function(hash){
+		$('#navigation a.active').removeClass('active');
+        $('#navigation a[href="' + window.location.hash + '"]').addClass('active');
+
+        $('#content .active').removeClass('active');
+        $('#content ' + hash).addClass('active');
+	};
+
+    smoothScroll.init({
+    	offset: 10,
+    	easing: "easeInOutCubic",
+    	callbackBefore: function (toggle, anchor) {
+    		activate(window.location.hash);
+    	}
+    });
+    if (window.location.hash == "" || window.location.hash == "#") {
+        activate('#title');
+    }
+    else {
+        activate(window.location.hash);
+    }
+    $('em:contains("git-vogue")').addClass('git-vogue-branding');
+    $('em:contains("Anchor")').addClass('anchor-branding');
+});
