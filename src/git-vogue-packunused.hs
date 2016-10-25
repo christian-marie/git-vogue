44b08ec60e5a2222799d91d5ba7b643e45f90f7c
Robin S. Krom <robin@digitalasset.com>
2016-10-25 18:31:12 +0200

    fix: packunused: read filepath's from file.

--- src/git-vogue-packunused.hs
+++ src/git-vogue-packunused.hs
@@ -25,7 +25,9 @@ main = f =<< getPluginCommand
   where
     f CmdName  = putStrLn "packunused"
 
-    f (CmdCheck check_fs all_fs) = do
+    f (CmdCheck check_fs_list all_fs_list) = do
+        check_fs <- read <$> readFile check_fs_list
+        all_fs <- read <$> readFile all_fs_list
         -- Grab all the projects dirs we want to traverse through
         rs <- forProjects (hsProjects check_fs all_fs) (const check)
         unless (and rs) exitFailure
